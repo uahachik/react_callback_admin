4e5135b015225f882dd021ae2011922fb8634247
Roman <kotenkoroman2019@gmail.com>
2019-03-30 01:28:19 +0200

    fix mistake

--- src/components/layout/AddRemark.js
+++ src/components/layout/AddRemark.js
@@ -73,12 +73,9 @@ class Addremark extends Component {
                 <input
                   className="fas fa-pencil-alt"
                   //  hidden
-<<<<<<< HEAD
                   type="Submit"
                   value="Add"
-=======
                   type="Add"
->>>>>>> 89ebbe5727bfb8f6bbd2fb4bca56affabe47d68b
                 />
               </form>
             </div>
